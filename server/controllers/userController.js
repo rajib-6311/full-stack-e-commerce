76c3f383c07e751f3c55f7dae85da8ed2a245483
rajib hossen <mhossen6311@gmail.com>
2025-08-13 18:34:22 +0600

    Update user authntication in server

---
+++ server/controllers/userController.js
@@ -0,0 +1,256 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt'
+import userModel from '../models/userModel.js';
+import jwt from 'jsonwebtoken'
+
+const createToken = (user) => {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+        name: user.name
+    },
+        process.env.JWT_SECRET,
+        { expiresIn: '24h' }
+    )
+}
+
+const userRegister = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // Request body verification
+        if (!name) {
+            return res.json({
+                success: false,
+                message: 'Name is required'
+            })
+        }
+        if (!email) {
+            return res.json({
+                success: false,
+                message: 'Email is required'
+            })
+        }
+        if (!password) {
+            return res.json({
+                success: false,
+                message: 'Password is required'
+            })
+        }
+
+        // email validation
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: 'Please enter a valid email'
+            })
+        }
+
+        // Cheek user status 
+        const existingUser = await userModel.findOne({ email });
+        if (existingUser) {
+            return res.json({ success: true, message: 'User already exist' })
+        }
+
+        // password validation
+        if (password.length < 8) {
+            return res.json({
+                success: true,
+                message: 'Give password more than 8 charter'
+            })
+        }
+        // Hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const encryptedPassword = await bcrypt.hash(password, salt)
+
+        // Register a new user
+        const newUser = new userModel({
+            name,
+            email,
+            password: encryptedPassword,
+        })
+
+        // save the user in database
+        await newUser.save()
+
+        res.json({
+            success: true,
+            message: 'Users register successfully'
+        })
+
+    } catch (error) {
+        console.log('User register ERROR', error)
+        res.json({ success: true, message: error?.error })
+    }
+};
+
+const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.json({
+                success: false,
+                message: 'Email is required'
+            })
+        }
+
+        // If user exist 
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "User doesn't exist"
+            })
+        }
+
+        if (!password) {
+            return res.json({
+                success: false,
+                message: 'Password is required'
+            })
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (isMatch) {
+            const token = createToken(user)
+            res.json({
+                success: true,
+                token,
+                message: 'Users logged in successfully'
+            })
+        }
+        else {
+            return res.json({
+                success: false,
+                message: 'Invalid credentials, try again'
+            })
+        }
+    } catch (error) {
+        console.log('User login ERROR', error)
+        res.json({ success: true, message: error?.error })
+    }
+};
+const adminLogin = async (req, res)=>{
+    try{
+        const {email, password} = req.body;
+
+        if(
+            email === process.env.ADMIN_EMAIL &&
+            password === process.env.ADMIN_PASSWORD
+        ){
+             const token = jwt.sign(email + password, process.env.JWT_SECRET);
+             res.json({
+                success:true,
+                 token,
+                 message: 'welcome admin user'
+             })
+        }
+        else{
+            res.json({
+              success: false,
+              message: 'Invalid credentials'
+            })
+        }
+
+    }catch(error){
+        console.log('Admin Login error', error)
+        res.json({
+            success: false,
+            message:error.message
+        })
+    }
+
+}
+const removeUser = async (req, res)=>{
+    try{
+   await userModel.findByIdAndDelete(req.body._id);
+   res.json({
+    success: true,
+    message: 'User deleted successfully'
+   })
+    }catch(error){
+        console.log('Removed user error', error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+const updateUser = async (req, res)=>{
+    try{
+     const {_id, email, password, name} = req.body;
+     const user = await userModel.findById(_id);
+
+     if(!user){
+        return res.json({
+            success:false,
+            message: 'User not found'
+        })
+     }
+     if(name) user.name = name;
+     if(email){
+        if (!validator.isEmail(email)) {
+            return res.json({
+                success: false,
+                message: 'Please enter a valid email'
+            })
+        }
+        user.email = email;
+     }
+
+     if(password){
+         if (password.length < 8) {
+            return res.json({
+                success: true,
+                message: 'Give password more than 8 charter'
+            })
+        }
+         // Hashing user password
+        const salt = await bcrypt.genSalt(10)
+        user.password = await bcrypt.hash(password, salt)
+     }
+
+    //  updating the user 
+    await user.save();
+    res.json({
+        success:true,
+        message: 'User update successfully'
+    })
+    }catch(error){
+        console.log('Update user error', error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+const getUser = async (req, res)=>{
+    try{
+        const total = await userModel.countDocuments({});
+        const users = await userModel.find({});
+
+        res.json({
+            success:true,
+            total,
+            users 
+        })
+
+    }catch(error){
+        console.log('Get all user error', error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export{
+    userRegister,
+    userLogin,
+    adminLogin,
+    removeUser,
+    updateUser,
+    getUser,
+}
---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const cors = require('cors')
+const { default: dbConnection } = require('./config/mongodb.js')
+const { default: userRouter } = require('./routes/userRouter.js')
+
+
+require('dotenv').config()
+const app = express()
+
+
+const port = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+
+// MongoDB connection 
+dbConnection()
+
+
+app.get('/', (req, res) => {
+  res.send('Full stack Server is running..!')
+})
+
+// Route connection 
+app.use('/api/user', userRouter)
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
